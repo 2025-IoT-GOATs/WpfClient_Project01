02a945714880887089c8e9819a3a780544354bf8
Owl-jun <80896230+Owl-jun@users.noreply.github.com>
2025-05-12 20:51:54 +0900

    11

--- WpfClient_Project01/Client/App.xaml.cs
+++ WpfClient_Project01/Client/App.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Configuration;
+﻿using Client.ViewModels;
+using Client.Views;
+using System.Configuration;
 using System.Data;
 using System.Windows;
 
@@ -9,6 +11,13 @@ namespace Client
     /// </summary>
     public partial class App : Application
     {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            var viewModel = new MainViewModel();
+            var view = new MainView();
+            view.DataContext = viewModel;
+            view.ShowDialog();
+        }
     }
 
 }
---
+++ WpfClient_Project01/Client/ViewModels/MainViewModel.cs
@@ -0,0 +1,30 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Client.ViewModels
+{
+    public partial class MainViewModel : ObservableObject
+    {
+
+
+        public MainViewModel()
+        {
+        }
+
+        [ObservableProperty]
+        private string userInput;
+
+        [RelayCommand]
+        public void Login()
+        {
+            MessageBox.Show(userInput,"dd");
+        }
+
+    }
+}
