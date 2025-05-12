067c3d09357065e105f93f8c47edc9b7f35b06d2
Owl-jun <80896230+Owl-jun@users.noreply.github.com>
2025-05-12 21:36:03 +0900

    비동기추가

---
+++ WpfClient_Project01/Client/Models/SocketClient.cs
@@ -0,0 +1,56 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using MySqlX.XDevAPI;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client.Models
+{
+    public class SocketClient : ObservableObject
+    {
+        private TcpClient _client;
+        private NetworkStream _stream;
+
+        public event Action<string> MessageReceived;
+
+        public async Task ConnectAsync(string ip, int port)
+        {
+            _client = new TcpClient();
+            await _client.ConnectAsync(ip, port);
+            _stream = _client.GetStream();
+        }
+        public void StartReceiveLoop()
+        {
+            Task.Run(async () =>
+            {
+                var buffer = new byte[1024];
+                while (true)
+                {
+                    try
+                    {
+                        int len = await _stream.ReadAsync(buffer, 0, buffer.Length);
+                        if (len == 0) break; // 연결 끊김
+
+                        string msg = Encoding.UTF8.GetString(buffer, 0, len);
+                        MessageReceived?.Invoke(msg);
+                    }
+                    catch
+                    {
+                        break;
+                    }
+                }
+            });
+        }
+        public async Task SendAsync(string msg)
+        {
+            if (_stream == null) return;
+            byte[] data = Encoding.UTF8.GetBytes(msg);
+            await _stream.WriteAsync(data, 0, data.Length);
+        }
+
+    }
+}
--- WpfClient_Project01/Client/ViewModels/MainViewModel.cs
+++ WpfClient_Project01/Client/ViewModels/MainViewModel.cs
@@ -1,7 +1,10 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
+﻿using Client.Models;
+using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
+using MySqlX.XDevAPI;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,19 +14,40 @@ namespace Client.ViewModels
 {
     public partial class MainViewModel : ObservableObject
     {
-
-
-        public MainViewModel()
-        {
-        }
+        private readonly SocketClient _client = new();
+        private string _chatName;
 
         [ObservableProperty]
         private string userInput;
 
-        [RelayCommand]
-        public void Login()
+        public ObservableCollection<string> Logs { get; } = new();
+
+        public MainViewModel()
         {
-            MessageBox.Show(userInput,"dd");
+            _client.MessageReceived += OnMessageReceived;
+        }
+
+        private void OnMessageReceived(string msg)
+        {
+            App.Current.Dispatcher.Invoke(() => Logs.Add("[수신] " + msg));
+        }
+
+        [RelayCommand]
+        public async Task ConnectAsync()
+        {
+            await _client.ConnectAsync("127.0.0.1", 9000);
+            _client.StartReceiveLoop();
+            _chatName = UserInput;
+            await _client.SendAsync("CHAT " + UserInput + " \n");
+            Logs.Add("[시스템] 서버 연결됨");
+        }
+
+        [RelayCommand]
+        public async Task SendAsync()
+        {
+            await _client.SendAsync("CHAT "+_chatName +" "+ UserInput+" \n");
+            Logs.Add("[송신] " + UserInput);
+            UserInput = string.Empty;
         }
 
     }
